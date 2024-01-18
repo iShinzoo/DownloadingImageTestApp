0a37cf2420812338c7be3ddf64d2f49b73fdb43c
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-01-19 00:27:53 +0530

    adding edittext for link

--- app/src/main/java/com/example/imagetestapp/MainActivity.kt
+++ app/src/main/java/com/example/imagetestapp/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.imagetestapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnImage: Button
-    private lateinit var img: ImageView
+    private lateinit var img : ImageView
+    private lateinit var edt : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         btnImage = findViewById(R.id.btnImage)
         img = findViewById(R.id.img)
+        edt = findViewById(R.id.edt)
 
         btnImage.setOnClickListener {
             downloadImage()
@@ -24,8 +27,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun downloadImage() {
+        val imageUrl = edt.text.toString()
         Glide.with(this)
-            .load("https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg")
+            .load(imageUrl)
             .into(img)
     }
 }
